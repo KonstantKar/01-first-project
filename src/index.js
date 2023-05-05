429c1ab541e582c339dc8c034e4a48711b40fd09
Konstantin <KonstantKar@yandex.ru>
2023-05-05 14:48:29 +0300

    Add callback, subscribe, observer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import "./index.css";
-
 import reportWebVitals from "./reportWebVitals";
-import { rerenderEntireTree } from "./render";
-import appState from "./Redux/appState";
+import appState, { subscribe } from "./Redux/appState";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { addPost } from "./Redux/appState";
+import { updateNewPostChange } from "./Redux/appState";
+import { addNewMessage } from "./Redux/appState";
+import { updateNewMessageChange } from "./Redux/appState";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+let rerenderEntireTree = (appState) => {
+  root.render(
+    <React.StrictMode>
+      <App
+        appState={appState}
+        addPost={addPost}
+        updateNewPostChange={updateNewPostChange}
+        addNewMessage={addNewMessage}
+        updateNewMessageChange={updateNewMessageChange}
+      />
+    </React.StrictMode>
+  );
+};
 
 rerenderEntireTree(appState);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
