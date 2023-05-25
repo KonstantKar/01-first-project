5686ee85bd2d3107dd6b11604548d57e796479e5
Konstantin <KonstantKar@yandex.ru>
2023-05-25 17:11:47 +0300

    add followAPI and unfollowAPI to usersComp

--- src/Components/Users/UsersComp.jsx/UsersComp.jsx
+++ src/Components/Users/UsersComp.jsx/UsersComp.jsx
@@ -2,44 +2,23 @@ import React from "react";
 import s from "./UsersComp.module.css";
 import userPhoto from "../../../assets/images/ava.png";
 import { NavLink } from "react-router-dom";
-import axios from "axios";
+import { followAPI, unfollowAPI } from "../../../API/api";
 
 const UsersComp = (props) => {
   let follow = () => {
-    axios
-      .post(
-        `https://social-network.samuraijs.com/api/1.0/follow/${props.id}`,
-        {},
-        {
-          withCredentials: true,
-          headers: {
-            "API-KEY": "6b6ecac8-d7f7-4477-87bb-015031017fed",
-          },
-        }
-      )
-      .then((response) => {
-        if (response.data.resultCode === 0) {
-          props.follow(props.id);
-        }
-      });
+    followAPI.getAxiosFollow(props.id).then((data) => {
+      if (data.resultCode === 0) {
+        props.follow(props.id);
+      }
+    });
   };
 
   let unFollow = () => {
-    axios
-      .delete(
-        `https://social-network.samuraijs.com/api/1.0/follow/${props.id}`,
-        {
-          withCredentials: true,
-          headers: {
-            "API-KEY": "6b6ecac8-d7f7-4477-87bb-015031017fed",
-          },
-        }
-      )
-      .then((response) => {
-        if (response.data.resultCode === 0) {
-          props.unFollow(props.id);
-        }
-      });
+    unfollowAPI.getAxiosUnfollow(props.id).then((data) => {
+      if (data.resultCode === 0) {
+        props.unFollow(props.id);
+      }
+    });
   };
 
   return (
