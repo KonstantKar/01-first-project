100b30818a3296b5613f8d7f3b8d529bd25040cd
Konstantin <KonstantKar@yandex.ru>
2023-05-28 01:36:01 +0300

    add thunk to auth/me in header

--- src/Components/Header/HeaderContainer.jsx
+++ src/Components/Header/HeaderContainer.jsx
@@ -1,28 +1,38 @@
 import { useDispatch, useSelector } from "react-redux";
 import Header from "./Header";
-import { setAuthUserDataAC } from "../../Redux/authReducer";
+import {
+  getAuthAccountDataTC,
+  setAuthUserDataAC,
+} from "../../Redux/authReducer";
 import { useEffect, useState } from "react";
 import { authAPI } from "../../API/api";
 const HeaderContainer = () => {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.auth.data);
-  const [isAuth, setIsAuth] = useState(false);
+  const isAuth = useSelector((state) => state.auth.isAuth);
+
+  //Код до добавления thunk-creator-a
+  /* const [isAuth, setIsAuth] = useState(false); 
 
   const setAccountData = (loadData) => {
     dispatch(setAuthUserDataAC(loadData));
   };
 
-  const getAccountData = () => {
+  const getAuthAccountData = () => {
     authAPI.getAxiosMyAccount().then((data) => {
       if (data.resultCode === 0) {
         setAccountData(data.data);
         setIsAuth(true);
       }
     });
+  }; */
+
+  const getAuthAccountData = () => {
+    dispatch(getAuthAccountDataTC());
   };
 
   useEffect(() => {
-    getAccountData();
+    getAuthAccountData();
   }, []);
 
   return <Header data={data} isAuth={isAuth} />;
--- src/Redux/authReducer.js
+++ src/Redux/authReducer.js
@@ -1,8 +1,11 @@
 import produce from "immer";
+import { authAPI } from "../API/api";
 const SET_USER_DATA = "SET-USER-DATA";
+const SET_IS_AUTH = "SET-IS-AUTH";
 
 let initialState = {
   data: { id: null, email: null, login: null },
+  isAuth: false,
 };
 
 const authReducer = (state = initialState, action) => {
@@ -10,6 +13,10 @@ const authReducer = (state = initialState, action) => {
     case SET_USER_DATA:
       return produce(state, (draftState) => {
         draftState.data = action.loadData;
+      });
+    case SET_IS_AUTH:
+      return produce(state, (draftState) => {
+        draftState.isAuth = action.isAuth;
       });
     default:
       return state;
@@ -20,6 +27,24 @@ export const setAuthUserDataAC = (loadData) => {
   return {
     type: SET_USER_DATA,
     loadData: loadData,
+  };
+};
+
+export const setIsAuthAC = (isAuth) => {
+  return {
+    type: SET_IS_AUTH,
+    isAuth: isAuth,
+  };
+};
+
+export const getAuthAccountDataTC = () => {
+  return (dispatch) => {
+    authAPI.getAxiosMyAccount().then((data) => {
+      if (data.resultCode === 0) {
+        dispatch(setAuthUserDataAC(data.data));
+        dispatch(setIsAuthAC(true));
+      }
+    });
   };
 };
 
